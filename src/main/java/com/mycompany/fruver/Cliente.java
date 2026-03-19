08e612fc4959ac5d49a62b42fd8fcd0f98b49900
Sebastian Arias Ramirez <est.sebastian.ariasr@unimilitar.edu.co>
2026-03-19 10:41:17 -0500

    se hizo un comentario para hacer clon

---
+++ src/main/java/com/mycompany/fruver/Cliente.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.fruver;
+// para github
+public class Cliente {
+    private String nombre;
+    
+
+    public Cliente(String nombre) {
+        this.nombre = nombre;
+        
+    }
+     public void comprarProducto(Producto producto, int cantidad) {
+        System.out.println(nombre + " quiere comprar " + cantidad + " " + producto);
+        producto.venderProducto(cantidad);
+    }
+
+   
+    public void mostrarCliente() {
+        System.out.println("Cliente: " + nombre);
+    }
+}
