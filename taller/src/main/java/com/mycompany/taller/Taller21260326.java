d5bbeb90dcb39902eb85166fcedd72239803b71a
Nicoll Xiomara Jimenez Reyes <est.nicoll.jimenez@unimilitar.edu.co>
2026-03-26 09:53:38 -0500

    se define el nodo

--- taller/src/main/java/com/mycompany/taller/Taller21260326.java
+++ taller/src/main/java/com/mycompany/taller/Taller21260326.java
@@ -10,11 +10,22 @@ import java.util.*;
 
 
 public class Taller21260326 {
+        
 
+        static class Nodo {
+        int valor;
+        Nodo siguiente;
+        
+          Nodo(int valor) {
+            this.valor = valor;
+            this.siguiente = null;
+        }
+    }
+        
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//leer un entero n
-        int n = sc.nextInt();
+        
+
         
 
 
