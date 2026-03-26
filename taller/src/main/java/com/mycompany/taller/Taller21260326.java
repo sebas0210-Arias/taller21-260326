5fb8bea50d30b6a036e1fb1543455b536b76be91
Nicoll Xiomara Jimenez Reyes <est.nicoll.jimenez@unimilitar.edu.co>
2026-03-26 09:47:36 -0500

    leer un entero n

---
+++ taller/src/main/java/com/mycompany/taller/Taller21260326.java
@@ -0,0 +1,23 @@
+/*
+Sebastian Arias Ramirez
+ */
+
+package com.mycompany.taller;
+
+
+
+import java.util.*;
+
+
+public class Taller21260326 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//leer un entero n
+        int n = sc.nextInt();
+        
+
+
+
+    }
+}
