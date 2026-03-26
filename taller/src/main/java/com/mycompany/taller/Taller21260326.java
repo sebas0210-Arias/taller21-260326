a018841a7219ac595fc29a71bb1b9be831885bbb
Sebastian Arias Ramirez <est.sebastian.ariasr@unimilitar.edu.co>
2026-03-26 10:06:48 -0500

    se define el nodo

--- taller/src/main/java/com/mycompany/taller/Taller21260326.java
+++ taller/src/main/java/com/mycompany/taller/Taller21260326.java
@@ -25,6 +25,8 @@ public class Taller21260326 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
+        int n = sc.nextInt();
+        
 
         
 
