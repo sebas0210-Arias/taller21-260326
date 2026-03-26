758e8458a4d352e642a3ed729a4d62faeaa06d8d
Sebastian Arias Ramirez <est.sebastian.ariasr@unimilitar.edu.co>
2026-03-26 10:30:19 -0500

    se lee el entero n y se define el nodo

--- taller21-260326/src/main/java/Taller21260326.java
+++ taller21-260326/src/main/java/Taller21260326.java
@@ -1,7 +1,24 @@
 
+   import java.util.Scanner;
+
 public class Taller21260326 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    // Definición del nodo
+    static class Nodo {
+        int valor;
+        Nodo siguiente;
+
+        Nodo(int valor) {
+            this.valor = valor;
+            this.siguiente = null;
+        }
     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Nodo cabeza = null;
+        Nodo actual = null;    }
 }
