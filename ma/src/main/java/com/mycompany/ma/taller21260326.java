b2b61bac550f8dc9da6bfb4143ca5f2fede855b2
Sebastian Arias Ramirez <est.sebastian.ariasr@unimilitar.edu.co>
2026-03-26 10:21:01 -0500

    se define el entero n

---
+++ ma/src/main/java/com/mycompany/ma/taller21260326.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ma;
+
+/**
+ *
+ * @author Admin
+ */
+import java.util.Scanner;
+public class taller21260326 {
+
+    // Definición del nodo
+    static class Nodo {
+        int valor;
+        Nodo siguiente;
+
+        Nodo(int valor) {
+            this.valor = valor;
+            this.siguiente = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Nodo cabeza = null;
+        Nodo actual = null;
+
+    }
+}
