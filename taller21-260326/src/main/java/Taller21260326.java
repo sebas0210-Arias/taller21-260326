c440dac786605bed0c99cb53cfbbbb2b72119cf7
Sebastian Arias Ramirez <est.sebastian.ariasr@unimilitar.edu.co>
2026-03-26 10:42:25 -0500

    Se implementa la lista enlazada simple para encontrar el número faltante

--- taller21-260326/src/main/java/Taller21260326.java
+++ taller21-260326/src/main/java/Taller21260326.java
@@ -20,5 +20,24 @@ public class Taller21260326 {
         int n = sc.nextInt();
 
         Nodo cabeza = null;
-        Nodo actual = null;    }
+        Nodo actual = null;  
+        
+    for( int i=1; i <n-2;i++){
+        int num= sc.nextInt();
+         Nodo nuevo = new Nodo(num);
+         
+         if(cabeza==null){
+             cabeza=nuevo;
+             actual=nuevo;
+           
+         }else{
+             actual.siguiente=nuevo;
+             actual=nuevo;
+            
+         }
+
+    }
+        
+        
+    }
 }
