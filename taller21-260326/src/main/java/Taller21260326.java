f5ccdaf1b4f88347e9f3f989806d528fb53cf1d9
est.sebastian.ariasr@unimilitar.edu.co <sebas@Sebastian>
2026-03-31 18:28:02 -0500

    no me deja inicializar y me aparece un error de que no se puede porque no hay main class

--- taller21-260326/src/main/java/Taller21260326.java
+++ taller21-260326/src/main/java/Taller21260326.java
@@ -46,23 +46,24 @@ static class Lista {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+               int n = sc.nextInt();
 
         Lista lista = new Lista();
 
-        // leer n-1 números
+        // Leer n-1 números
         for (int i = 0; i < n - 1; i++) {
-            lista.insertar(sc.nextInt());
+            int num = sc.nextInt();
+            lista.insertar(num);
         }
-        int sumaLista = lista.sumar();
+        long sumaLista = lista.sumar();
 
         long sumaTotal = (long) n * (n + 1) / 2;
 
         long faltante = sumaTotal - sumaLista;
 
         System.out.println(faltante);
+
+        sc.close();
     }
 }
-//no inicializa correctamente
-        
-     
+//no me deja inicializar
